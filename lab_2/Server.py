1d4784cf86e21527340fcc52b711a4eeb4a245f0
Danil <Dankwaso@gmail.com>
2019-10-17 13:28:48 +0300

    Update Server.py

--- lab_2/Server.py
+++ lab_2/Server.py
@@ -7,7 +7,6 @@ from flask import Flask, render_template, send_from_directory, redirect, url_for
 
 app = Flask(__name__)
 
-@app.route('/')
 @app.route('/index')
 def server_run():
     url3 = "https://stopgame.ru/news"
@@ -23,10 +22,6 @@ def server_run():
 @app.route('/refresh', methods=['POST'])
 def save():
     return redirect(url_for('server_run'))
-
-@app.route('/favicon.ico') 
-def favicon(): 
-    return send_from_directory(os.path.join(app.root_path, 'static'), 'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
 if __name__ == '__main__':
     app.run()
