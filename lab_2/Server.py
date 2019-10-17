ed21874fcd044ddec5f4109a523027398171e4e2
Danil <Dankwaso@gmail.com>
2019-10-17 13:54:31 +0300

    Update Server.py

--- lab_2/Server.py
+++ lab_2/Server.py
@@ -7,6 +7,7 @@ from flask import Flask, render_template, send_from_directory, redirect, url_for
 
 app = Flask(__name__)
 
+@app.route('/')
 @app.route('/index')
 def server_run():
     url3 = "https://stopgame.ru/news"
